b6e4469e3939c57049b604936aad6480880fffd5
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-22 16:50:02 +0200

    added steps palidroma

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,24 @@
 console.log('JS OK')
+
+/* 
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+/*
+
+//1. Chiedere all'utente di inserire una parola con un form.
+    //1.b Recuperare l'elemento di interesse dal DOM.
+
+//2. Creare una funzione 
+
+//3. Scomporre la intera parola in lettere ovvero
+scomporre l'intera stringa per ciascuno dei caratteri.
+
+//4. creare un LOOP il quale inizia a contare dal ultimo carattere 
+per poter invertire l'ordine dei caratteri
+
+//5. Una volta invertito l'ordine dei caratteri unificare i caratteri in un stringa
+
+//6. Verificare che la parola invertita sia identica alla parola iniziale 
+
+*/
